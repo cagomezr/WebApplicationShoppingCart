56943502276e89744b7132342e701c01ba242859
cagomezr <carlos.a.gomez.romero@gmail.com>
2018-07-02 02:06:45 -0400

    BLL minor update
    BLL minor update  didnt go with initial Commit.

--- WebApplicationShoppingCart/App_Code/BLL/AddProducts.cs
+++ WebApplicationShoppingCart/App_Code/BLL/AddProducts.cs
@@ -9,15 +9,16 @@ namespace WebApplicationShoppingCart.App_Code.BLL
 {
     public class AddProducts
     {
-        public bool AddProduct()
+        public bool AddProduct(int cartID,int productID, int quantity)
         {
-            bool returnbool = false;
+            bool returnbool = true;
             try
             {
                 using (NorthwindEntities context = new NorthwindEntities())
                 {
+                    context.spAddProductToCart(cartID, productID, quantity);
                 }
-            }catch(Exception ex){}
+            }catch(Exception ex){returnbool = false; }
             return returnbool;
         }
     }
